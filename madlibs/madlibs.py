e5f65c3f0cfc71ffa87037d891f397cd05e9d750
arsummers <aliyasummers1@gmail.com>
2020-05-15 16:49:19 -0700

    creating and writing to new file

--- madlibs/madlibs.py
+++ madlibs/madlibs.py
@@ -1,4 +1,11 @@
-file = open('assets/template.txt')
+with open('assets/template.txt', 'r') as file:
+    contents = file.read()
 
-contents = file.read()
 print(contents)
+
+for i in contents:
+    if i == '{':
+        print('found the bracket')
+
+with open('assets/filled.txt', 'w') as file2:
+    file2.write(contents)
